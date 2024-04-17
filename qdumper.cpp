d6cb8ab84c75eaaea39e3e17165dd2036806dd1d
Andrey <98462616+kuzk0@users.noreply.github.com>
2024-04-17 21:52:26 +0300

    [DEV-612] Передача параметров подключения к облаку через QDumper (#4)
    FIX: path to dumps parameter is QString now
    FIX: cv2pdb is now a mozilla repo release version (x64 only)
    
    ADD: config parameter in QDumper() and DumpUploader
    
    ADD: lambda callback function to delete dump file after it's uploaded
    
    DEL: QDumper::deleteDump

--- qdumper.cpp
+++ qdumper.cpp
@@ -5,46 +5,49 @@
 
 #include "src/qcrashhandler/qcrashhandler.h"
 
-QDumper::QDumper(std::optional<QString> *reportPath, bool dDAUFlag, QObject *parent)
-	: QObject{parent}
+QDumper::QDumper(QString reportPath, bool dDAUFlag, ConnectionConfig config, QObject *parent)
+    : QObject{parent}
 {
-	QDir dumpsDir = createDumpsDir(reportPath);
+    dumpUploader = new DumpUploader(config, this);
 
-	Breakpad::CrashHandler::instance()->Init(dumpsDir.absolutePath());
+    QDir dumpsDir = createDumpsDir(reportPath);
+    Breakpad::CrashHandler::instance()->Init(dumpsDir.absolutePath());
 
-	deleteDumpAfterUploadFlag = dDAUFlag;
-	checkAndUploadDump(dumpsDir);
+    deleteDumpAfterUploadFlag = dDAUFlag;
+    checkAndUploadDump(dumpsDir);
 }
 
-QDir QDumper::createDumpsDir(std::optional<QString> *dumpsDirPath)
+QDir QDumper::createDumpsDir(QString dumpsDirPath)
 {
-	QDir dumpsDir;
-	if (dumpsDirPath) {
-		dumpsDir = QDir(dumpsDirPath->value());
-		return dumpsDir;
-	}
+    QDir dumpsDir;
+    if (dumpsDirPath.isEmpty()) {
+        dumpsDir = QDir(dumpsDirPath);
+        return dumpsDir;
+    }
 
-	dumpsDir = QDir(QStandardPaths::standardLocations(QStandardPaths::AppLocalDataLocation).constFirst());
-	if (dumpsDir.mkpath("crashdumps")) {
-		dumpsDir.cd("crashdumps");
-	}
+    dumpsDir = QDir(QStandardPaths::standardLocations(QStandardPaths::AppLocalDataLocation).constFirst());
+    if (dumpsDir.mkpath("crashdumps")) {
+        dumpsDir.cd("crashdumps");
+    }
 
-	return dumpsDir;
+    return dumpsDir;
 }
 
-void QDumper::checkAndUploadDump(QDir &dumpsDir)
+void QDumper::checkAndUploadDump(QDir dumpsDir)
 {
-	QStringList dumpList = createDumpfileList(dumpsDir);
+    QStringList dumpList = createDumpfileList(dumpsDir);
 
-	if (dumpList.isEmpty()) {
-		return;
-	}
+    if (dumpList.isEmpty()) {
+        return;
+    }
 
-	if (dumpUploader.uploadfile(dumpsDir.absoluteFilePath(dumpList.constFirst()))) {
-		if (deleteDumpAfterUploadFlag) {
-			deleteDump(dumpsDir, dumpList.constFirst());
-		}
-	}
+    QString dumpPath = dumpList.constFirst();
+    dumpUploader->uploadfile(dumpsDir.absoluteFilePath(dumpPath),
+                             [this, dumpsDir, dumpPath]() mutable {
+                                 if (deleteDumpAfterUploadFlag) {
+                                     dumpsDir.remove(dumpPath);
+                                 }
+                             });
 }
 
 QStringList QDumper::createDumpfileList(QDir &dumpsDir)
@@ -52,13 +55,6 @@ QStringList QDumper::createDumpfileList(QDir &dumpsDir)
 	dumpsDir.setNameFilters(QStringList("*.dmp"));
 	dumpsDir.setSorting(QDir::Time);
 	return dumpsDir.entryList();
-}
-
-void QDumper::deleteDump(QDir &dumpsDir, const QString &dumpPath)
-{
-	if (deleteDumpAfterUploadFlag) {
-		dumpsDir.remove(dumpPath);
-	}
 }
 
 QDumper::~QDumper()
